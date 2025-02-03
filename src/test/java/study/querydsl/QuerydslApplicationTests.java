491b230ef36e1a0b4311e10f9ee54d758322b8ed
dong <dho0647@naver.com>
2025-02-04 06:26:56 +0900

    init

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,34 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import entity.Hello;
+import entity.QHello;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query.selectFrom(qHello).fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+    }
+
+}
