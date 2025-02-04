b284a7f75ecf97affb9c73e99087a862d11a1c09
dong <dho0647@naver.com>
2025-02-05 05:56:47 +0900

    예제 도메인 모델과 동작확인

---
+++ src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,52 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+@NamedQuery(
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username=:username"
+)
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    // 실무에서는 지연로딩 세팅, 즉시로딩은 성능튜닝이 어려워진다.
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        // 객체이기 때문에 나만 변경하는게 아니라 반대쪽 team도 변경해줘야 한다.
+        team.getMembers().add(this);
+    }
+}
---
+++ src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,29 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    // mappedBy 기준은 외래키가 없는쪽에서 거는게 좋다.
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
